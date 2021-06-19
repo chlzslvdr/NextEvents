406e684525f6f542425356cafa5215aef49e3f59
Chelzea Salvador <chelzeasalvador@gmail.com>
2021-06-19 23:28:58 +0800

    Adding Buttons & Icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import React from "react";
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <title>NextEvents</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
