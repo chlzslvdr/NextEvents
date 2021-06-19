9566565abd35b3b9b8ed412935e92a97a52142ef
Chelzea Salvador <chelzeasalvador@gmail.com>
2021-06-19 22:54:47 +0800

    Adding More React Components & Connecting Components

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,36 @@
+import Link from "next/link";
+import Image from "next/image";
 function EventItem(props) {
-  const { items } = props;
-  return <li></li>;
+  const { title, image, date, location, id } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li>
+      <Image src={image} alt={title} />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>{humanReadableDate}</time>
+          </div>
+          <div>
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default EventItem;
--- components/events/event-list.js
+++ components/events/event-list.js
@@ -2,10 +2,18 @@ import EventItem from "./event-item";
 
 function EventList(props) {
   const { items } = props;
+
   return (
     <ul>
       {items.map((event) => (
-        <EventItem key={event.id} />
+        <EventItem
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          location={event.location}
+          date={event.date}
+          image={event.image}
+        />
       ))}
     </ul>
   );
