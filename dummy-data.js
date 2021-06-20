d0162795d08b7872e58fb8f8a4888a35599f93af
Chelzea Salvador <chelzeasalvador@gmail.com>
2021-06-20 12:52:09 +0800

    additional dummy data

--- dummy-data.js
+++ dummy-data.js
@@ -4,7 +4,7 @@ const DUMMY_EVENTS = [
     title: "Programming for everyone",
     description:
       "Everyone can learn to code! Yes, everyone! In this live event, we are going to go through all the key basics and get you started with programming as well.",
-    location: "Somestreet 25, 12345 San Somewhereo",
+    location: "971  Mahlon Street, West Roxbury, MA",
     date: "2021-04-12",
     image: "/images/coding-event.jpg",
     isFeatured: false,
@@ -14,20 +14,40 @@ const DUMMY_EVENTS = [
     title: "Networking for introverts",
     description:
       "We know: Networking is no fun if you are an introvert person. That's why we came up with this event - it'll be so much easier. Promised!",
-    location: "New Wall Street 5, 98765 New Work",
+    location: "1621  Brannon Avenue, High Island, TX",
     date: "2021-05-31",
     image: "/images/introvert-event.jpg",
     isFeatured: true,
   },
   {
     id: "3",
+    title: "Consumer Electronics Shows",
+    description:
+      "CES has been the global gathering place for all those involved in consumer technologies and the next generation of innovations, showcasing products in the areas of 3D Printing, Augmented & Virtual Reality, Computer Hardware/Software/Services, and more.",
+    location: "2019  Maple Avenue, Las Vegas, NV",
+    date: "2021-10-08",
+    image: "/images/electronics-event.jpg",
+    isFeatured: true,
+  },
+  {
+    id: "4",
     title: "Networking for extroverts",
     description:
       "You probably need no help with networking in general. But focusing your energy correctly - that is something where most people can improve.",
-    location: "My Street 12, 10115 Broke City",
+    location: "307  Cherry Camp Road, Winchester, NY",
     date: "2022-03-15",
     image: "/images/extrovert-event.jpg",
     isFeatured: true,
+  },
+  {
+    id: "5",
+    title: "Gartner Digital Workplace Summit",
+    description:
+      "At the Gartner Digital Workplace Summit, the world’s leading IT advisory company offers you the chance to be the first to hear about emerging trends and latest predictions surrounding the future of work, provides an action plan to build the digital workplace of tomorrow, and offers real-world solutions for leveraging these technologies your organization.",
+    location: "3692  Mayo Street, Los Angeles, CA",
+    date: "2022-07-11",
+    image: "/images/workplace-event.jpg",
+    isFeatured: false,
   },
 ];
 
