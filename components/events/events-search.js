766b81873b52880bc688fb2ab4cbd3c4927a756d
Chelzea Salvador <chelzeasalvador@gmail.com>
2021-06-21 08:59:55 +0800

    Navigating to the Filtered Events Page Progammatically

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,32 +1,45 @@
+import { useRef } from "react";
 import Button from "../ui/button";
 import classes from "./events-search.module.css";
 
 function EventsSearch(props) {
+  const yearInputref = useRef();
+  const monthInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const selectedYear = yearInputref.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
-            <options value="2021">2021</options>
-            <options value="2022">2022</options>
+          <select id="year" ref={yearInputref}>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
-            <options value="1">January</options>
-            <options value="2">February</options>
-            <options value="3">March</options>
-            <options value="4">April</options>
-            <options value="5">May</options>
-            <options value="6">June</options>
-            <options value="7">July</options>
-            <options value="8">August</options>
-            <options value="9">September</options>
-            <options value="10">October</options>
-            <options value="11">November</options>
-            <options value="12">December</options>
+          <select id="month" ref={monthInputRef}>
+            <option value="1">January</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
           </select>
         </div>
       </div>
