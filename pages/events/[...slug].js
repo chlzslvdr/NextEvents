6651e910045da42df20676c67671bd4c7256d8df
Chelzea Salvador <chelzeasalvador@gmail.com>
2021-06-21 11:29:57 +0800

    enhance error msg ui and polish components

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,5 +1,11 @@
+import { Fragment } from "react";
 import { useRouter } from "next/router";
-import { getFeaturedEvents } from "../../dummy-data";
+import { getFilteredEvents } from "../../dummy-data";
+import ResultsTitle from "../../components/events/results-title";
+import EventList from "../../components/events/event-list";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
+
 function FilteredEventsPage() {
   const router = useRouter();
 
@@ -23,22 +29,55 @@ function FilteredEventsPage() {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return <p>Invalid filter. Please adjust your values!</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
-  const filteredEvents = getFeaturedEvents({
+  const filteredEvents = getFilteredEvents({
     year: numYear,
     month: numMonth,
   });
 
   if (!filteredEvents || filteredEvents.length === 0) {
-    return <p>No events found for the chosen filter!</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter!</p>
+        </ErrorAlert>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
+  const date = new Date(numYear, numMonth - 1);
+
   return (
-    <div>
-      <h1>Filtered Events</h1>
-    </div>
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filteredEvents} />
+    </Fragment>
   );
 }
 
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,6 +4,7 @@ import { getEventById } from "../../dummy-data";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
 
 function EventDetailPage() {
   const router = useRouter();
@@ -11,7 +12,13 @@ function EventDetailPage() {
 
   const event = getEventById(eventId);
   if (!event) {
-    return <p>No event found!</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No event found!</p>
+        </ErrorAlert>
+      </Fragment>
+    );
   }
 
   return (
