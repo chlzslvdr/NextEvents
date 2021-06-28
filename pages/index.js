1caade1a953f4f2171b367285b6aad15ad0cb3cb
Chelzea Salvador <chelzeasalvador@gmail.com>
2021-06-28 13:30:21 +0800

    Configuring head content

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
+import Head from "next/head";
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
 
 function HomePage(props) {
-  return <EventList items={props.events} />;
+  return (
+    <div>
+      <Head>
+        <title>NextEvents</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allows you to evolve..."
+        />
+      </Head>
+      <EventList items={props.events} />
+    </div>
+  );
 }
 
 export async function getStaticProps() {
