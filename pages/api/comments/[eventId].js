37487289c06094c72ecbb95a6d142aaf8f35d5b4
Chelzea Salvador <chelzeasalvador@gmail.com>
2021-06-30 11:13:37 +0800

    Adding Comments API routes

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,47 @@
+import { v4 as uuidv4 } from "uuid";
+
+function handler(req, res) {
+  const eventId = req.query.eventId;
+
+  if (req.method === "POST") {
+    const { email, name, text } = req.body;
+
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({
+        message: "Invalid input.",
+      });
+      return;
+    }
+
+    const newComment = {
+      id: uuidv4(),
+      email,
+      name,
+      text,
+    };
+
+    console.log(newComment);
+    res.status(201).json({
+      message: "Added comment.",
+      comment: newComment,
+    });
+  }
+
+  if (req.method === "GET") {
+    const dummyList = [
+      { id: "c1", name: "Max", text: "A first comment!" },
+      { id: "c2", name: "Manuel", text: "A second comment!" },
+    ];
+
+    res.status(200).json({
+      comments: dummyList,
+    });
+  }
+}
+export default handler;
