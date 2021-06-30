8a602b74037747379c690124ed72dc39766ba339
Chelzea Salvador <chelzeasalvador@gmail.com>
2021-06-30 10:50:17 +0800

    Adding a Newsletter Route

---
+++ pages/api/newsletter.js
@@ -0,0 +1,19 @@
+function handler(req, res) {
+  if (req.method === "POST") {
+    const userEmail = req.body.email;
+
+    if (!userEmail || !userEmail.includes("@")) {
+      res.status(422).json({
+        message: "Invalid email address.",
+      });
+      return;
+    }
+
+    console.log("userEmail", userEmail);
+    res.status(201).json({
+      message: "Signed up!",
+    });
+  }
+}
+
+export default handler;
