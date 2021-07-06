f45d5ea9c1e51d971c1aea0510f3d4052360f821
Chelzea Salvador <chelzeasalvador@gmail.com>
2021-07-06 13:29:17 +0800

    finished

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,9 +1,9 @@
 import { useContext } from "react";
 import classes from "./notification.module.css";
-// import NotificationContext from "../../store/notification-context";
+import NotificationContext from "../../store/notification-context";
 
 function Notification(props) {
-  // const notificationCtx = useContext(NotificationContext);
+  const notificationCtx = useContext(NotificationContext);
 
   const { title, message, status } = props;
 
@@ -24,10 +24,7 @@ function Notification(props) {
   const activeClasses = `${classes.notification} ${statusClasses}`;
 
   return (
-    <div
-      className={activeClasses}
-      // onClick={notificationCtx.hideNotification}
-    >
+    <div className={activeClasses} onClick={notificationCtx.hideNotification}>
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
--- store/notification-context.js
+++ store/notification-context.js
@@ -1,13 +1,29 @@
-import { useState, createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NotificationContext = createContext({
-  notfication: null, // {title, message, status}
+  notification: null, // { title, message, status }
   showNotification: function (notificationData) {},
   hideNotification: function () {},
 });
 
 export function NotificationContextProvider(props) {
   const [activeNotification, setActiveNotification] = useState();
+
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === "success" ||
+        activeNotification.status === "error")
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
 
   function showNotificationHandler(notificationData) {
     setActiveNotification(notificationData);
